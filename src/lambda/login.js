6dcdf6b965f4ed3faa117816ff8f1c71e833e280
Dimmi <pechunka11@gmail.com>
2019-07-30 21:37:13 +0200

    sdfsdf

--- src/lambda/login.js
+++ src/lambda/login.js
@@ -8,24 +8,24 @@ import User from './models/user.model'
 exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
 
-    const {email, password} = JSON.parse(event.body)
+  const {email, password} = JSON.parse(event.body)
 
-    const user = await User.findOne({email: email, password: password})
+  const user = await User.findOne({email: email, password: password})
 
-    if(user){
-      const response = {
-        msg: "User successfully found",
-        error: false,
-        data: user
-      }
-      return {
-        statusCode: 200,
-        body: JSON.stringify(response)
-      }
-    }else{
-      return {
-        statusCode: 500,
-        body: JSON.stringify({msg: 'error', error: true})
-      }
+  if(user){
+    const response = {
+      msg: "User successfully found",
+      error: false,
+      data: user
     }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
+  }else{
+    return {
+      statusCode: 500,
+      body: JSON.stringify({msg: 'error', error: true})
+    }
+  }
 }

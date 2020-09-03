20a5600910543739655d8b0590dd6a5d2832e153
Дмитро Печунка <pechunka11@gmail.com>
2020-09-03 22:08:47 +0200

    fix login and registration

--- src/lambda/login.js
+++ src/lambda/login.js
@@ -8,24 +8,33 @@ import User from './models/user.model'
 exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
 
-  const {email, password} = JSON.parse(event.body)
+  try{
+    const {email, password} = JSON.parse(event.body)
 
-  const user = await User.findOne({email: email, password: password})
+    const user = await User.findOne({email: email, password: password})
 
-  if(user){
-    const response = {
-      msg: "User successfully found",
-      error: false,
-      data: user
+    if(user){
+      const response = {
+        msg: "User successfully found",
+        error: false,
+        data: user
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      }
+    }else{
+      return {
+        statusCode: 500,
+        body: JSON.stringify({msg: 'error', error: true})
+      }
     }
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response)
-    }
-  }else{
+  }catch(err) {
+    console.log('data.login', err) // output to netlify function log
     return {
       statusCode: 500,
-      body: JSON.stringify({msg: 'error', error: true})
+      body: JSON.stringify({msg: err.message})
     }
   }
+
 }

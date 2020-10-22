492876648c8ae69a41efeea24b3dae4366c5ae73
simon1400 <pechunka11@gmail.com>
2020-10-22 11:08:16 +0200

    create swipe menu, fixed filter

--- src/app/components/sub-menu/index.js
+++ src/app/components/sub-menu/index.js
@@ -12,26 +12,7 @@ if(window.location.pathname.split('/')[1] === 'en'){
   lang = 'cz'
 }
 
-
-function useWindowSize() {
-  const [size, setSize] = useState([0, 0]);
-  useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-  return size;
-}
-
-// active_sub
-
 const SubMenu = ({data, articles = false}) => {
-
-  const [responseMenu, setResponseMenu] = useState(false)
-  const [width] = useWindowSize();
 
   const [baseUrl, setBaseUrl] = useState('')
 
@@ -43,29 +24,9 @@ const SubMenu = ({data, articles = false}) => {
     }
   }, [])
 
-  useEffect(() => {
-    reespMenu()
-  }, [data.length])
-
-  useEffect(() => {
-    reespMenu()
-  }, [width])
-
-  const reespMenu = () => {
-    let allItems = Object.values(document.querySelectorAll(".sub_menu_item"))
-    if(allItems.length > 1){
-      var sumWidth = allItems.reduce((a, b) => a + b.offsetWidth, 0)
-      sumWidth += (allItems.length - 1) * 80
-      if(window.innerWidth - 80 <= sumWidth){
-        setResponseMenu(true)
-      }else{
-        setResponseMenu(false)
-      }
-    }
-  }
 
   return(
-    <nav className={`sub_menu ${responseMenu ? "sub_menu_responsive" : ''}`}>
+    <nav className="sub_menu">
       <ul>
         {window.location.pathname.split('/')[1] === 'produkty' || window.location.pathname.split('/')[2] === 'produkty'
           ? <li uk-filter-control="" className={`sub_menu_item`}><Link to="#catalog-short" scroll={el => el.scrollIntoView({behavior: "smooth", block: "start"})}>Všechny produkty</Link></li> : ''}

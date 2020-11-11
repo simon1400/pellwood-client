61e5a307c86a53612f256c19e1a01754864d0ceb
simon1400 <pechunka11@gmail.com>
2020-11-11 15:54:40 +0100

    Initial commit

--- src/app/restClient.js
+++ src/app/restClient.js
@@ -9,33 +9,3 @@ export const AxiosAPI = axios.create({
     'Access-Control-Allow-Origin': '*'
   }
 })
-
-
-// // Add a response interceptor
-// AxiosAPI.interceptors.response.use((response) => {
-//   // Any status code that lie within the range of 2xx cause this function to trigger
-//   // Do something with response data
-//   return response;
-// }, function (error) {
-//   // Any status codes that falls outside the range of 2xx cause this function to trigger
-//   // Do something with response error
-//   console.log('error.response', error.response)
-//   if (error?.response?.status === 401) {
-//     localStorage.clear();
-//     window.location.href = AUTH_URL
-//   }
-//   return Promise.reject(error)
-// })
-//
-// AxiosAPI.interceptors.request.use((request) => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     request.headers['Authorization'] = 'Bearer ' + token
-//   }
-//   return request;
-// }, function (error) {
-//   // Any status codes that falls outside the range of 2xx cause this function to trigger
-//   // Do something with response error
-//   console.log(error)
-//   return Promise.reject(error)
-// })
